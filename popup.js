905fb4b2205605feca6a5787c295d2ceca2b29bd
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-10-22 10:40:11 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({null,
+    chrome.tabs.executeScript({
     code: `(${ inContent })()`
   }, ([result] = []) => {
     if (!chrome.runtime.lastError) {
