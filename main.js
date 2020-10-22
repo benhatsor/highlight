b7b38e5e2dd0e73dcea9a2ac4595e841143944fd
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-10-22 10:00:54 +0300

    Update and rename bookmark.html to main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript({
+    code: `(${ inContent })()`
+  }, ([result] = []) => {
+    if (!chrome.runtime.lastError) {
+      chrome.bookmarks.create({'parentId': bookmarkBar.id, 'title': result.title, 'url': result.url+'?scroll='+scrollPos });
+    }
+  });
+});
+
+function inContent() {
+  return {
+    success: true,
+    title: document.title,
+    url: window.location.href,
+    scrollPos: window.scrollY || window.pageYOffset
+  };
+}
