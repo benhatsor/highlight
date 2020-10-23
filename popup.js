e222500301dc7dd587a76ee7644fe03c0414e948
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-10-23 14:37:52 +0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -8,51 +8,6 @@ function inContent() {
     };
 }
 
-function findOrCreateDestinationFolder(rootNodes) {
-    var rootNode;
-    if (rootNodes.length > 0) {
-        rootNode = rootNodes[0];
-    }
-    destFolder = findBookmarksFolder(rootNode, "Smart Bookmarks");
-    if (!destFolder) {
-        bookmarkBar = findBookmarksFolder(rootNode, "Other bookmarks");
-        browser.bookmarks.create({
-            parentId: bookmarkBar ? bookmarkBar.id : "1",
-            title: "Smart Bookmarks"
-        }, function(bmk) {
-            destFolder = bmk;
-            createBookmark(pageData);
-        });
-    } else {
-        createBookmark(pageData);
-    }
-}
-
-function createBookmark(result) {
-    if (destFolder) {
-        browser.bookmarks.create({
-            'parentId': destFolder.id,
-            'title': result.title,
-            'url': (result.url + '#' + Math.round(result.scrollPos))
-        });
-    }
-}
-
-function findBookmarksFolder(rootNode, searchString) {
-    if (rootNode.url) {
-        return null;
-    } else if (rootNode.title.indexOf(searchString) >= 0) {
-        return rootNode;
-    }
-    for (var i = 0; i < rootNode.children.length; i++) {
-        var dest = findBookmarksFolder(rootNode.children[i], searchString);
-        if (dest) {
-            return dest;
-        }
-    }
-    return null;
-}
-
 function select(node) {
     node.focus();
     if (document.body.createTextRange) {
@@ -87,13 +42,7 @@ browser.tabs.executeScript({
 		
 		document.querySelector('.save').addEventListener('click', e => {
 		  pageData.title = document.querySelector('.title').innerHTML;
-		  
-		  if (isFirefox) {
-			  toggleBookmark();
-		  }
-		  else {
-			  browser.bookmarks.getTree(findOrCreateDestinationFolder);
-		  }
+		  toggleBookmark();
 		  window.close();
 		})
 		
