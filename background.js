cacb9e3069ab6984f0856413c4ee6f4663e575fb
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-10-24 10:25:52 +0300

    Add files via upload

--- background.js
+++ background.js
@@ -1,17 +1,25 @@
-browser.tabs.onUpdated.addListener(function() {  
+browser.tabs.onUpdated.addListener(function() {
   browser.tabs.executeScript({
     code: `(${ scrollInContent })()`
   });
 })
 
-browser.tabs.onActivated.addListener(function() {
-  browser.browserAction.setIcon({path: 'icon.png'});
-})
-
 function scrollInContent() {
   var hash = window.location.hash.replace('#', '');
-  
+
   if (hash && !isNaN(hash)) {
-    window.scroll({top: Number(hash) })
+    window.scroll({
+      top: Number(hash)
+    })
   }
+}
+
+if (window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  browser.browserAction.setIcon({
+    path: {
+      "48": "icon-dark.png",
+      "96": "icon-dark@2x.png"
+    }
+  });
 }
--- popup.js
+++ popup.js
@@ -1,57 +1,75 @@
-var destFolder, bookmarkBar, pageData;
+var pageData;
 
 function inContent() {
-    return {
-        title: window.location.hostname,
-        url: window.location.href.split('#')[0],
-        scrollPos: window.scrollY || window.pageYOffset
-    };
+  var body = document.body,
+    html = document.documentElement;
+
+  var height = Math.max(body.scrollHeight, body.offsetHeight,
+    html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+  return {
+    title: window.location.hostname,
+    url: window.location.href.split('#')[0],
+    scrollPos: window.scrollY || window.pageYOffset,
+    pageHeight: height
+  };
 }
 
 function select(node) {
-    node.focus();
-    if (document.body.createTextRange) {
-        const range = document.body.createTextRange();
-        range.moveToElementText(node);
-        range.select();
-    } else if (window.getSelection) {
-        const selection = window.getSelection();
-        const range = document.createRange();
-        range.selectNodeContents(node);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
+  node.focus();
+  if (document.body.createTextRange) {
+    const range = document.body.createTextRange();
+    range.moveToElementText(node);
+    range.select();
+  } else if (window.getSelection) {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(node);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
 }
 
 function toggleBookmark() {
   browser.bookmarks.create({
     title: pageData.title,
-    url: pageData.url+'#'+Math.round(pageData.scrollPos)
+    url: pageData.url + '#' + Math.round(pageData.scrollPos)
   });
 }
 
 browser.tabs.executeScript({
-    code: `(${ inContent })()`
+  code: `(${ inContent })()`
 }, ([result] = []) => {
-    if (!browser.runtime.lastError) {
-		pageData = result;
-		
-		document.querySelector('.title').innerHTML = pageData.title;
-		document.querySelector('.url').innerHTML = pageData.url;
-		select(document.querySelector('.title'));
-		
-		document.querySelector('.save').addEventListener('click', e => {
-		  pageData.title = document.querySelector('.title').innerHTML;
-		  toggleBookmark();
-		  window.close();
-		})
-		
-		document.querySelector('.cancel').addEventListener('click', e => {
-		  browser.browserAction.setIcon({path: 'icon.png'}, function() {
-		    window.close();
-		  });
-		})
-    }
-});
+  if (!browser.runtime.lastError) {
+    pageData = result;
 
-browser.browserAction.setIcon({path: 'icon-on.png'});
+    var scrollPos = pageData.scrollPos;
+    var fourth = pageData.pageHeight / 4;
+    var place = '';
+    if (scrollPos > 0 && scrollPos < fourth) {
+      place = ' - near top';
+    } else if (scrollPos > fourth && scrollPos < (fourth * 2)) {
+      place = ' - near middle';
+    } else if (scrollPos > (fourth * 2) && scrollPos < (fourth * 3)) {
+      place = ' - near middle';
+    } else if (scrollPos > (fourth * 3) && scrollPos < pageData.pageHeight) {
+      place = ' - near bottom';
+    } else if (scrollPos == pageData.pageHeight) {
+      place = ' - bottom';
+    }
+
+    document.querySelector('.title').innerHTML = pageData.title + place;
+    document.querySelector('.url').innerHTML = pageData.url;
+    select(document.querySelector('.title'));
+
+    document.querySelector('.save').addEventListener('click', e => {
+      pageData.title = document.querySelector('.title').innerHTML;
+      toggleBookmark();
+      window.close();
+    })
+
+    document.querySelector('.cancel').addEventListener('click', e => {
+      window.close();
+    })
+  }
+});
